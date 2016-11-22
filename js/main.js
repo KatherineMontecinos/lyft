38f9073551572fcc61c1e3715f6dfc7506dd99d8
Katherine Montecinos <katherine.montecinos.k@gmail.com>
2016-11-22 13:40:29 -0300

    funcionalidad del input y detalles en el html

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ $(function() {
     $('#' + $(this).val()).show();
   });
 });
+
+
+/*var carpetas = $("#renombra");
+$('option', carpetas).remove();
+
+for(var i=0; i<res.length; i++)
+{
+var img = getimg(res[i]);
+var ruta = "img/"+img+"";
+
+carpetas.append('<option style="background:url('+ruta+') no-repeat center left; padding-left:20px;" value='+ruta+res[i]+' >'+res[i]+'</option>');
+}*/
