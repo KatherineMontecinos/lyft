7299b98ff33f058f58d1abc7eeef392389e86b4b
katherine montecinos <katherine.montecinos.k@gmail.com>
2016-11-24 22:43:14 -0300

    funcion de las banderas ok, cambios en html y css

--- js/main.js
+++ js/main.js
@@ -1,25 +1,53 @@
 //function validarCodigo() {
 	// rescatar el valor del input
-	//var inputCodigo = $("#codigo").val();
 
-
-    // crear una función 
-
-$(function() {
+/*$(function() {
   $('#codeselector').change(function(){
     $('.code').hide();
     $('#' + $(this).val()).show();
   });
-});
+});*/
 
+var paises = [
+	{
+		nombre: "Chile",
+		prefijo: "+569",
+		bandera: "img/chile.png"
+	},
+	{
+		nombre: "Mexico",
+		prefijo: "+529",
+		bandera: "img/mexico.png"
+	},
+	{
+		nombre: "Peru",
+		prefijo: "+519",
+		bandera: "img/peru.png"
+	},
+	{
+		nombre: "Estados Unidos",
+		prefijo: "+1",
+		bandera: "img/eeuu.png"
+	},
+	{
+		nombre: "Argentina",
+		prefijo: "+549",
+		bandera: "img/argentina.png"
+	},
+];
 
-/*var carpetas = $("#renombra");
-$('option', carpetas).remove();
+// traer el nombre del pais desde localStorage
+var nombre_pais = window.localStorage.getItem("nombre_pais") || "Chile";
 
-for(var i=0; i<res.length; i++)
-{
-var img = getimg(res[i]);
-var ruta = "img/"+img+"";
+// recuperar el objeto pais, desde el arreglo de paises
+var pais = {};
+for (var i=0 ; i<paises.length ; ++i){
+	if (paises[i].nombre == nombre_pais) {
+		pais = paises[i];
+		break;
+	}
+}
+// Modificar el DOM
+$('#pais img').attr('src', pais.bandera);
+$('#prefijo').html(pais.prefijo);
 
-carpetas.append('<option style="background:url('+ruta+') no-repeat center left; padding-left:20px;" value='+ruta+res[i]+' >'+res[i]+'</option>');
-}*/
